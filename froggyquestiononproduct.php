2144911287a82d14318fb24fec94250ca8e8d802
Alain Folletete <folletete.alain@gmail.com>
2014-12-19 15:22:58 +0100

    Update module version

--- froggyquestiononproduct.php
+++ froggyquestiononproduct.php
@@ -55,7 +55,7 @@ class FroggyQuestionOnProduct extends FroggyModule
 	{
 		$this->name = 'froggyquestiononproduct';
 		$this->author = 'Froggy Commerce';
-		$this->version = '1.1.2';
+		$this->version = '1.2.0';
 		$this->tab = 'front_office_features';
 
 		parent::__construct();
