36c2d1a9f29fc466417c875e207c2311d29ff880
Fabien Serny <fabien@23prod.fr>
2015-09-22 17:56:40 +0200

    Replace tabs with 4 spaces

--- form.php
+++ form.php
@@ -32,65 +32,65 @@ $module = new FroggyQuestionOnProduct();
 $errors = $assign = array();
 
 if (Configuration::get('FC_QOP_ONLY_FOR_CUSTOMER') && !$module->isCustomerLogged())
-	$errors[] = Tools::displayError('Please login, in order to send a question about a product');
+    $errors[] = Tools::displayError('Please login, in order to send a question about a product');
 else
 {
-	if (Tools::isSubmit('submitQuestion'))
-	{
-		if (Validate::isInt(Tools::getValue('id_product')))
-		{
-			$product = new Product(Tools::getValue('id_product'));
-			if (!Validate::isLoadedObject($product))
-				$errors[] = $module->l('Product ID is incorrect');
-		}
-		else
-			$errors[] = $module->l('Product ID is incorrect');
+    if (Tools::isSubmit('submitQuestion'))
+    {
+        if (Validate::isInt(Tools::getValue('id_product')))
+        {
+            $product = new Product(Tools::getValue('id_product'));
+            if (!Validate::isLoadedObject($product))
+                $errors[] = $module->l('Product ID is incorrect');
+        }
+        else
+            $errors[] = $module->l('Product ID is incorrect');
 
-		if (!$module->isCustomerLogged() && !Validate::isEmail(Tools::getValue('email')))
-			$errors[] = $module->l('Your email is invalid');
+        if (!$module->isCustomerLogged() && !Validate::isEmail(Tools::getValue('email')))
+            $errors[] = $module->l('Your email is invalid');
 
-		if (!Validate::isCleanHtml(Tools::getValue('message')) || !Tools::getValue('message'))
-			$errors[] = $module->l('Message field is invalid');
+        if (!Validate::isCleanHtml(Tools::getValue('message')) || !Tools::getValue('message'))
+            $errors[] = $module->l('Message field is invalid');
 
-		if (!count($errors))
-		{
-			/*
-			 * Create Customer Thread
-			 */
-			$ct = new CustomerThread();
-			if (isset($context->customer->id) && $context->customer->id)
-				$ct->id_customer = $context->customer->id;
-			$ct->id_shop = (int)$context->shop->id;
-			$ct->id_product = Tools::getValue('id_product');
-			$ct->id_contact = Configuration::get('FC_QOP_CONTACT_ID');
-			$ct->id_lang = (int)$context->language->id;
-			if ($module->isCustomerLogged())
-				$ct->email = $context->customer->email;
-			else
-				$ct->email = Tools::getValue('email');
+        if (!count($errors))
+        {
+            /*
+             * Create Customer Thread
+             */
+            $ct = new CustomerThread();
+            if (isset($context->customer->id) && $context->customer->id)
+                $ct->id_customer = $context->customer->id;
+            $ct->id_shop = (int)$context->shop->id;
+            $ct->id_product = Tools::getValue('id_product');
+            $ct->id_contact = Configuration::get('FC_QOP_CONTACT_ID');
+            $ct->id_lang = (int)$context->language->id;
+            if ($module->isCustomerLogged())
+                $ct->email = $context->customer->email;
+            else
+                $ct->email = Tools::getValue('email');
 
-			$ct->status = 'open';
-			$ct->token = Tools::passwdGen(12);
-			if ($ct->add())
-			{
-				/*
-				 * Prepare message
-				 */
-				$message = $module->l('A customer have a question about one product...');
-				$message .= "\n\n".'---'."\n\n";
-				$message .= Tools::htmlentitiesUTF8(Tools::getValue('message'));
+            $ct->status = 'open';
+            $ct->token = Tools::passwdGen(12);
+            if ($ct->add())
+            {
+                /*
+                 * Prepare message
+                 */
+                $message = $module->l('A customer have a question about one product...');
+                $message .= "\n\n".'---'."\n\n";
+                $message .= Tools::htmlentitiesUTF8(Tools::getValue('message'));
 
-				$cm = new CustomerMessage();
-				$cm->id_customer_thread = $ct->id;
-				$cm->message = $message;
-				$cm->ip_address = ip2long($_SERVER['REMOTE_ADDR']);
-				$cm->user_agent = $_SERVER['HTTP_USER_AGENT'];
-				if ($cm->add())
-				{
-					$assign = array('success' => true);
+                $cm = new CustomerMessage();
+                $cm->id_customer_thread = $ct->id;
+                $cm->message = $message;
+                $cm->ip_address = ip2long($_SERVER['REMOTE_ADDR']);
+                $cm->user_agent = $_SERVER['HTTP_USER_AGENT'];
+                if ($cm->add())
+                {
+                    $assign = array('success' => true);
 
-					$customer_id_lang = $context->language->id;
-					$context->language->id = Configuration::get('PS_LANG_DEFAULT');
+                    $customer_id_lang = $context->language->id;
+                    $context->language->id = Configuration::get('PS_LANG_DEFAULT');
 
                     Mail::Send(Configuration::get('PS_LANG_DEFAULT'), 'new-question', $module->l('A new question about a product has been asked to you'), array(
                         '{product_id}' => $product->id,
@@ -100,24 +100,24 @@ else
                         '{question}' => Tools::htmlentitiesUTF8(Tools::getValue('message'))
                     ), Configuration::get('PS_SHOP_EMAIL'), null, $ct->email, null, null, null, _PS_MODULE_DIR_.'/'.$module->name.'/mails/', false);
 
-					$context->language->id = $customer_id_lang;
-				}
-				else
-					$errors[] = Tools::displayError('An error occurred while sending the message.');
-			}
-			else
-				$errors[] = Tools::displayError('An error occurred while sending the message.');
-		}
+                    $context->language->id = $customer_id_lang;
+                }
+                else
+                    $errors[] = Tools::displayError('An error occurred while sending the message.');
+            }
+            else
+                $errors[] = Tools::displayError('An error occurred while sending the message.');
+        }
 
-		if (Tools::getIsset('ajax'))
-		{
-			echo Tools::jsonEncode(array(
-				'has_errors' => (bool)count($errors),
-				'errors' => $errors
-			));
-			exit;
-		}
-	}
+        if (Tools::getIsset('ajax'))
+        {
+            echo Tools::jsonEncode(array(
+                'has_errors' => (bool)count($errors),
+                'errors' => $errors
+            ));
+            exit;
+        }
+    }
 }
 
 $product = new Product(Tools::getValue('id_product'), false, $context->language->id);
